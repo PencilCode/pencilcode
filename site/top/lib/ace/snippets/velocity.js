618917faf893356c72ffb4b602216de4832a90fa
David Bau <david.bau@gmail.com>
2014-01-01 19:35:54 -0500

    Switch to a branch of ace editor that supports mobile clients.

--- site/top/lib/ace/snippets/velocity.js
+++ site/top/lib/ace/snippets/velocity.js
@@ -1,1 +1,1 @@
-define("ace/snippets/velocity",["require","exports","module"],function(e,t,n){t.snippetText="",t.scope="velocity"})
+ace.define("ace/snippets/velocity",["require","exports","module"],function(e,t,n){t.snippetText="",t.scope="velocity"})
