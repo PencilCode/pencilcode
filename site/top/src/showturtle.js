d1b9fbd8355a3c047e563cac073e0d9c98889ca7
David Bau <david.bau@gmail.com>
2014-05-20 07:54:33 -0400

    Add comment.

--- site/top/src/showturtle.js
+++ site/top/src/showturtle.js
@@ -1,6 +1,9 @@
 $.turtle();
 if (window.ide == null) try {
   window.ide = null;
+  // Propagate the ide variable from the parent window, if present.
+  // When running framed inside in the pencilcode ide, this object
+  // is set up by editor-debug.js.
   if (window.parent !== window && window.parent.ide &&
       $.isFunction(window.parent.ide.getEditorText)) {
     window.ide = window.parent.ide;
