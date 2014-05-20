91129748bcbef2237460150e6a4d420711352aac
David Bau <david.bau@gmail.com>
2014-05-20 12:15:40 -0400

    Provide a standard ide object to libraries.

--- site/top/src/showturtle.js
+++ site/top/src/showturtle.js
@@ -1,1 +1,8 @@
 $.turtle();
+if (window.ide == null) try {
+  window.ide = null;
+  if (window.parent !== window && window.parent.ide &&
+      $.isFunction(window.parent.ide.getEditorText)) {
+    window.ide = window.parent.ide;
+  }
+} catch (e) {}
