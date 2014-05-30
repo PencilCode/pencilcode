9e5cba03fdd5bac925a99010dcaff5d1d3809b99
David Bau <david.bau@gmail.com>
2014-05-29 21:01:23 -0400

    Add test exercising username validation.

---
+++ test/new_user.js
@@ -0,0 +1,175 @@
+var phantom = require('node-phantom-simple'),
+    phantomjs = require('phantomjs'),
+    assert = require('assert'),
+    testutil = require('./lib/testutil'),
+    one_step_timeout = 8000,
+    refreshThen = testutil.refreshThen,
+    asyncTest = testutil.asyncTest;
+
+describe('new user', function() {
+  var _ph, _page;
+  before(function(done) {
+    // Create the headless webkit browser.
+    phantom.create(function(error, ph) {
+      assert.ifError(error);
+      // Open a page for browsing.
+      _ph = ph;
+      _ph.createPage(function(err, page) {
+        _page = page;
+        // Set the size to a modern laptop size.
+        page.set('viewportSize', { width: 1200, height: 900 }, function(err) {
+          assert.ifError(err);
+          // Point it to a blank page to start
+          page.open('about:blank', function(err, status){
+            assert.ifError(err);
+            assert.equal(status, 'success');
+            done();
+          });
+        });
+      });
+    }, {
+      // Launch phantomjs from the phantomjs package.
+      phantomPath: phantomjs.path,
+      parameters: {
+        // Use the test server as a proxy server, so that all requests
+        // go to this server (instead of trying real DNS lookups).
+        proxy: '127.0.0.1:8193',
+        // Set the disk storage to zero to avoid persisting localStorage
+        // between test runs.
+        'local-storage-quota': 0
+      }
+    });
+  });
+  after(function() {
+    // Be sure to kill the browser when the test is done, or else
+    // we can leave orphan processes.
+    _ph.exit();
+  });
+  it('should serve static editor HTML', function(done) {
+    // Visit an example for anonymous users.
+    _page.open('http://pencilcode.net.dev/edit/first',
+        function(err, status) {
+      assert.ifError(err);
+      assert.equal(status, 'success');
+      _page.evaluate(function() {
+        // Inject a script that clears the login cookie for a clean start.
+        document.cookie='login=; expires=Thu, 01 Jan 1970 00:00:00 GMT; path=/';
+        // And also clear localStorage for this site.
+        localStorage.clear();
+      }, function(err) {
+        assert.ifError(err);
+        done();
+      });
+    });
+  });
+  it('should load code', function(done) {
+    asyncTest(_page, one_step_timeout, null, null, function() {
+      // Poll until the element with class="editor" appears on the page.
+      if (!$('.editor').length) return;
+      // Reach in and return the text that is shown within the editor.
+      var ace_editor = ace.edit($('.editor').attr('id'));
+      return {
+        text: ace_editor.getSession().getValue(),
+        active: document.activeElement &&
+            document.activeElement.getAttribute('class'),
+        preview: $('.preview').length,
+        saved: $('#save').prop('disabled'),
+        login: $('#login').length,
+        logout: $('#logout').length
+      };
+    }, function(err, result) {
+      assert.ifError(err);
+      // The editor text should contain this line of code.
+      assert.ok(/for \[1..25\]/.test(result.text));
+      // The save button should not be disabled (even though unmodified).
+      assert.equal(result.saved, false);
+      // There should be no login or logout buton.
+      assert.equal(result.login, 0);
+      assert.equal(result.logout, 0);
+      // The element with focus should be the editor
+      assert.equal(result.active, 'ace_text-input');
+      done();
+    });
+  });
+  it('should show login prompt when saving', function(done) {
+    asyncTest(_page, one_step_timeout, null, function() {
+      // Click on the save button.
+      $('#save').click();
+    }, function() {
+      // Wait for the login dialog to pop up.
+      if (!$('.dialog').is(':visible')) return;
+      return {
+        itext: $('.dialog .info').html(),
+        udisabled: $('.username').is(':disabled'),
+        uval: $('.username').val(),
+        pdisabled: $('.password').is(':disabled'),
+        pval: $('.password').val()
+      };
+    }, function(err, result) {
+      assert.ifError(err);
+      // The login dialog should link to terms of service and privacy policy.
+      assert.ok(/href="\/privacy.html\"/.test(result.itext));
+      assert.ok(/href="\/terms.html\"/.test(result.itext));
+      // The username should be enabled and start empty.
+      assert.equal(false, result.udisabled);
+      assert.equal('', result.uval);
+      // The password should be enabled and start empty.
+      assert.equal(false, result.pdisabled);
+      assert.equal('', result.pval);
+      done();
+    });
+  });
+  it('should reject a bad username', function(done) {
+    asyncTest(_page, one_step_timeout, null, function() {
+      // Enter a low-complexity username, and click OK.
+      $('.username').val('zkkg');
+      $('.username').trigger('keyup');
+    }, function() {
+      // Wait for the info prompt to show a message without "privacy.html"
+      if (!$('.info').is(':visible')) return;
+      if ($('.info').text().match(/privacy.html/)) return;
+      return {
+        infotext: $('.info').text()
+      };
+    }, function(err, result) {
+      assert.ifError(err);
+      // The message should report that the password is wrong.
+      assert.equal('Name \"zkkg\" reserved.', result.infotext);
+      done();
+    });
+  });
+  it('should accept an OK username', function(done) {
+    asyncTest(_page, one_step_timeout, null, function() {
+      // Enter the correct password, and click OK again.
+      $('.username').val('zkgtest');
+      $('.username').trigger('keyup');
+    }, function() {
+      // Wait for the info prompt to show a message without "zkkg"
+      if (!$('.info').is(':visible')) return;
+      if ($('.info').text().match(/zkkg/)) return {poll:true, msg:$('.info').text()};
+      return {
+        infotext: $('.info').text()
+      };
+    }, function(err, result) {
+      assert.ifError(err);
+      // The butterbar should show that the document is saved.
+      assert.ok(/Will create zkgtest/.test(result.infotext));
+      done();
+    });
+  });
+  it('is done', function(done) {
+    asyncTest(_page, one_step_timeout, null, function() {
+      // Final cleanup: delete local storage and the cookie.
+      localStorage.clear();
+      document.cookie='login=; expires=Thu, 01 Jan 1970 00:00:00 GMT; path=/';
+    }, function() {
+      return {
+        cookie: document.cookie
+      };
+    }, function(err, result) {
+      assert.ifError(err);
+      assert.ok(!/login=/.test(result.cookie));
+      done();
+    });
+  });
+});
---
+++ testdata/first
@@ -0,0 +1,5 @@
+speed 2
+pen red
+for [1..25]
+  fd 100
+  rt 88
