99dc3e3c00a4c393798285efa849d1d9ed0a77df
David Bau <david.bau@gmail.com>
2014-07-16 17:54:15 -0400

    Add noturtle attribute support for turtlebits script.

--- site/top/src/showturtle.js
+++ site/top/src/showturtle.js
@@ -1,11 +1,15 @@
-$.turtle();
-if (window.ide == null) try {
-  window.ide = null;
-  // Propagate the ide variable from the parent window, if present.
-  // When running framed inside in the pencilcode ide, this object
-  // is set up by editor-debug.js.
-  if (window.parent !== window && window.parent.ide &&
-      $.isFunction(window.parent.ide.getEditorText)) {
-    window.ide = window.parent.ide;
-  }
-} catch (e) {}
+(function() {
+  var scripts = document.getElementsByTagName('script'),
+      thisScript = scripts.length && scripts[scripts.length - 1];
+  if (!thisScript.hasAttribute('noturtle')) { $.turtle(); }
+  if (window.ide == null) try {
+    window.ide = null;
+    // Propagate the ide variable from the parent window, if present.
+    // When running framed inside in the pencilcode ide, this object
+    // is set up by editor-debug.js.
+    if (window.parent !== window && window.parent.ide &&
+        'function' == typeof(window.parent.ide.getEditorText)) {
+      window.ide = window.parent.ide;
+    }
+  } catch (e) {}
+})();
