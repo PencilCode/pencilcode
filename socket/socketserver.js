154f5ddfcc154b7fe5b715110f40ec1be4236656
David Bau <david.bau@gmail.com>
2014-10-20 17:53:43 +0000

    Add simple socket.io reflector.

---
+++ socket/socketserver.js
@@ -0,0 +1,17 @@
+var io = require('socket.io')();
+io.on('connect', function(socket) {
+  var host = socket.handshake.headers.host.split(':').shift();
+  var room = socket.handshake.query.room || '';
+  var hroom = host + '-' + room;
+  socket.join(hroom);
+  socket.onevent = function(packet) {
+    var args = packet.data || [];
+    if (null != packet.id) {
+      args.push(this.ack(packet.id));
+    }
+    var s = socket.nsp.to(hroom);
+    s.emit.apply(s, args);
+  }
+});
+io.listen(process.env.PORT);
+console.log('Socket.io server listening on ' + process.env.PORT);
