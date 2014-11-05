f0688c99d4d9897d29b7765f6563b919ee1a3251
David Bau <david.bau@gmail.com>
2014-11-05 13:02:43 -0500

    Update socket server to support reflecting namespaces.

--- server/socket.js
+++ server/socket.js
@@ -1,32 +1,42 @@
 // A standalone socket.io reflector server.
 
-var io = require('socket.io')(),
+var DynamicServer = require('./dynamic.io'),
+    debug = require('debug')('reflector-socket.io'),
     config = require('./config');
 
-io.on('connect', function(socket) {
-  // All connections on the same hostname will talk to each other,
-  // but the room=X url parameter can be supplied to set a specific room.
-  var host = socket.handshake.headers.host.split(':').shift();
-  var room = socket.handshake.query.room || '';
-  var hroom = host + '-' + room;
-  socket.join(hroom);
-
-  // Overriding onevent allows us to reflect every type of packet.
-  socket.onevent = function(packet) {
-    var args = packet.data || [];
-    if (null != packet.id) {
-      args.push(this.ack(packet.id));
-    }
-    var s = socket.nsp.to(hroom);
-    console.log('sending', args, 'to', hroom);
-    s.emit.apply(s, args);
-  }
-
+io = DynamicServer({
+  mainHost: process.env.HOST,     // Enable virtual host handling
+  publicStatus: true              // Enable /socket.io/status page.
 });
+io.setupNamespace('*', function(nsp) {
+  nsp.on('connect', function(socket) {
+    debug('connecting socket %s in nsp %s', socket.id, socket.nsp.fullname());
+    // All connections on the same host will talk to each other,
+    // but the room=X url parameter can be supplied to set a specific room.
+    var room = socket.handshake.query.room || null;
+    if (room) {
+      socket.join(room);
+    }
 
+    // Overriding onevent allows us to reflect every type of packet.
+    socket.onevent = function(packet) {
+      debug('got', packet);
+      var args = packet.data || [];
+      if (null != packet.id) {
+        args.push(this.ack(packet.id));
+      }
+      var s = socket.nsp;
+      if (room) {
+        s = s.to(room);
+      }
+      debug('reflecting', args, 'to', socket.nsp.fullname(), room || '');
+      s.emit.apply(s, args);
+    }
+  });
+});
 io.listen(process.env.PORT);
 
 if (config.gid) { process.setgid(config.gid); }
 if (config.uid) { process.setuid(config.uid); }
 console.log('Socket.io server listening on ' + process.env.PORT +
-    ' with uid ' + process.getuid());
+    ' (' + process.env.HOST + ') with uid ' + process.getuid());
