a2851bbe4000b5701d780d582e558d5990a59445
Caitlyn <caitsydney@gmail.com>
2015-02-13 10:43:30 -0500

    Fix changing n to x

--- content/src/palette.js
+++ content/src/palette.js
@@ -193,7 +193,7 @@ return {
           block: 'label \'spot\'',
           title: 'Write text at the turtle'
         }, {
-          block: 'read \'?\', (x) ->\x  write \'Hello\' + x',
+          block: 'read \'?\', (x) ->\n  write \'Hello\' + x',
           title: 'Read input from the user'
         }, {
           block: 'readnum \'Side\', (n) ->\n  write \'A\' + n * n',
