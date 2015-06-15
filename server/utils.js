6d15a1c4c1dc623aebe6935960ffb33df7169829
Liu Xinan <xinan@me.com>
2015-06-16 00:15:38 +0800

    Revert fixing typo and fix folder name in test data

--- server/utils.js
+++ server/utils.js
@@ -110,7 +110,7 @@ function makeAbsolute(filename, app) {
   return path.resolve(absfile);
 };
 
-var THUMB_DIR = '.thumbs/';
+var THUMB_DIR = '.thumb/';
 
 exports.getAbsThumbPath = function(filename, app) {
   return makeAbsolute(makeThumbPath(filename), app);
