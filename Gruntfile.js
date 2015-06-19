36962e132e5211d5539c00c1587bbebe7f6fd75d
Jeremy Ruten <jeremy.ruten@gmail.com>
2015-06-19 10:01:23 -0600

    Update pencil-tracer.js
    Also, add 'useStrict: true' option to requirejs compile options, since
    pencil-tracer includes a javascript parser that contains 'use strict;'
    strings that should not be stripped.

--- Gruntfile.js
+++ Gruntfile.js
@@ -87,6 +87,7 @@ module.exports = function(grunt) {
           deps: ['src/editor-main'],
           name: 'lib/almond',
           out: 'content/editor.js',
+          useStrict: true,
           // optimize: 'none',
           mainConfigFile: 'content/src/editor-main.js',
           preserveLicenseComments: false
