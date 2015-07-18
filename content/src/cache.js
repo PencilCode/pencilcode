5f60b5ede1efd4b889b21a345f166a1c0a9e8cef
Dinuka <dinuka.2013513@iit.ac.lk>
2015-07-18 09:41:15 +0530

    New Cache module

---
+++ content/src/cache.js
@@ -0,0 +1,77 @@
+///////////////////////////////////////////////////////////////////////////
+// CACHE SUPPORT
+///////////////////////////////////////////////////////////////////////////
+
+var $              = require('jquery'),
+    filetype       = require('filetype'),
+    tooltipster    = require('tooltipster'),
+    see            = require('see'),
+    droplet        = require('droplet-editor'),
+    palette        = require('palette'),
+    codescan       = require('codescan'),
+    drawProtractor = require('draw-protractor'),
+    ZeroClipboard  = require('ZeroClipboard'),
+    FontLoader     = require('FontLoader');
+
+eval(see.scope('cache'));
+
+var cache = window.pencilcode.cacheObj = {};
+
+var model = window.pencilcode.cache = {
+  put:put,
+  get:get,
+  delete:deleteCache
+};
+
+/**
+ * Initialize the cache object to the given
+ * name only if it's not available already.
+ * 
+ * @param {string} name
+ * @returns {undefined}
+ */
+function initializeCache(name) {
+  if (!cache[name]) {
+      cache[name] = {};
+  }
+}
+
+/**
+ * Delete the cache object mapped with the given name
+ * 
+ * @param {string} name
+ * @returns {undefined}
+ */
+function deleteCache(name) {
+  delete cache[name];
+}
+
+/**
+ * Return the cached value mapped with the given name and key
+ * 
+ * @param {string} cacheName
+ * @param {string} key
+ * @returns {unresolved}
+ */
+function get(cacheName,key) {
+    if (cache[cacheName]) {
+        return cache[cacheName][key];
+    }
+    return null;
+}
+
+/**
+ * Save the given value mapped with the given name and key
+ * 
+ * @param {type} cacheName
+ * @param {type} key
+ * @returns {undefined}
+ */
+function put(cacheName,key,value) {
+    initializeCache(cacheName);
+    cache[cacheName][key] = value;
+}
+
+
+
+module.exports = model;
