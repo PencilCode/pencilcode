05c95fa22cbac74043ad795645e3bba5f8b36dda
Liu Xinan <xinan@me.com>
2015-07-20 20:42:32 +0800

    Add `html2canvas` into `bower.json` and bowercopy task

--- Gruntfile.js
+++ Gruntfile.js
@@ -27,6 +27,7 @@ module.exports = function(grunt) {
           'lib/coffee-script.js': 'coffee-script/extras/coffee-script.js',
           'lib/droplet.js': 'droplet/dist/droplet-full.js',
           'lib/droplet.css': 'droplet/css/droplet.css',
+          'lib/html2canvas.js': 'html2canvas/dist/html2canvas.js',
           'lib/iced-coffee-script.js':
              'iced-coffee-script/extras/iced-coffee-script-1.8.0-c.js',
           'lib/jquery.js' : 'jquery/dist/jquery.js',
