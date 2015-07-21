1a7018fa8940273f1d32754ebb926a40f419d09f
David Bau <david.bau@gmail.com>
2015-07-20 23:57:42 -0400

    update to latest libraries.

--- content/lib/droplet.js
+++ content/lib/droplet.js
@@ -2,7 +2,7 @@
  * Copyright (c) 2015 Anthony Bau.
  * MIT License.
  *
- * Date: 2015-07-18
+ * Date: 2015-07-20
  */
 (function(f){if(typeof exports==="object"&&typeof module!=="undefined"){module.exports=f()}else if(typeof define==="function"&&define.amd){define([],f)}else{var g;if(typeof window!=="undefined"){g=window}else if(typeof global!=="undefined"){g=global}else if(typeof self!=="undefined"){g=self}else{g=this}g.droplet = f()}})(function(){var define,module,exports;return (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
 // Generated from C.g4 by ANTLR 4.5
@@ -67022,6 +67022,9 @@ TAGS = {
   u: {
     category: 'content'
   },
+  center: {
+    category: 'content'
+  },
   sub: {
     category: 'content'
   },
@@ -70746,7 +70749,7 @@ exports.Parser = Parser = (function() {
     for (i = k = 0, len1 = lines.length; k < len1; i = ++k) {
       line = lines[i];
       if (!(i in markupOnLines)) {
-        if (indentDepth >= line.length || line.slice(0, indentDepth).trim().length > 0) {
+        if (indentDepth > line.length || line.slice(0, indentDepth).trim().length > 0) {
           head.specialIndent = ((function() {
             var l, ref1, results;
             results = [];
@@ -70774,7 +70777,7 @@ exports.Parser = Parser = (function() {
         }
         head = helper.connect(head, new model.NewlineToken());
       } else {
-        if (indentDepth >= line.length || line.slice(0, indentDepth).trim().length > 0) {
+        if (indentDepth > line.length || line.slice(0, indentDepth).trim().length > 0) {
           lastIndex = line.length - line.trimLeft().length;
           head.specialIndent = line.slice(0, lastIndex);
         } else {
