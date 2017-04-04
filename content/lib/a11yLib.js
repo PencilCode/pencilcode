75d42d9a7defb3f899bb275fc12064424623f8b0
caseycarroll42 <aiyakc@gmail.com>
2017-04-04 13:20:52 -0500

    remove let statement because it creates an error in the uglify process

--- content/lib/a11yLib.js
+++ content/lib/a11yLib.js
@@ -10,14 +10,14 @@ function _init() {
 //create an array of primary navigation sections on the site
 function setupPrimaryNav() {
     //main ui sections
-    let blockEditor = document.querySelector('.droplet-main-scroller')
-    let blockCategory = document.querySelector('.droplet-palette-header')    
-    let runButton = document.querySelector('#run')
+    var blockEditor = document.querySelector('.droplet-main-scroller')
+    var blockCategory = document.querySelector('.droplet-palette-header')    
+    var runButton = document.querySelector('#run')
 
     //console exists in seperate iframe, so get the scope of the iframe to select it
-    let outputFrame = document.querySelector('#output-frame')
-    let outputDocument = outputFrame.contentDocument || outputFrame.contentWindow.document    
-    let consoleInput = outputDocument.querySelector('._log #_testinput') //select console
+    var outputFrame = document.querySelector('#output-frame')
+    var outputDocument = outputFrame.contentDocument || outputFrame.contentWindow.document    
+    var consoleInput = outputDocument.querySelector('._log #_testinput') //select console
 
     //listed in navigation order
     const primaryNavSections = [
