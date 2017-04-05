f938989b3f0c4eec564b62cb2c6ab0bfd0cfbc2a
caseycarroll42 <aiyakc@gmail.com>
2017-04-05 15:39:15 -0500

    convert a11yLib features to a class

--- content/lib/a11yLib.js
+++ content/lib/a11yLib.js
@@ -1,28 +1,25 @@
-function _init() {
-    document.activeElement.blur(); //set focus to body of view    
-    
-    document.getElementsByClassName("droplet-wrapper-div")[0].setAttribute("id", "maincontent");
-    // document.querySelector(".skip").setAttribute("tabindex", 0);
-    
-    //load primary nav sections
-    const primaryNavSections = setupPrimaryNav();
+function a11yController() {
+    this.primaryNavSections = this.setupPrimaryNav();
+    this.init();
+}
 
+a11yController.prototype.init = function () {
+    document.activeElement.blur(); //set focus to body of view     
+    
+    //set target for skip link
+    document.getElementsByClassName("droplet-wrapper-div")[0].setAttribute("id", "maincontent");
+    
     //remove iframes from tab index
     var iframes = document.querySelectorAll('iframe');    
     iframes.forEach(function(element) {        
         element.setAttribute("tabindex", -1);
-    }, this);
-    
-    //remove tab index from primary navigation so we can handle this manually
-    primaryNavSections.forEach(function(element) {
-        element.setAttribute("tabindex", -1);
-    })
+    }, this);        
 
     //intercept keyboard events
-    document.addEventListener('keydown', function (event) {tabController(event)});
+    document.addEventListener('keydown', this.tabController);
 }
 
-function tabController(event) {    
+a11yController.prototype.tabController = function (event) {
     if(event.keyCode == 9) {
         //SHIFT + TAB 
         if(event.shiftKey) {            
@@ -36,13 +33,11 @@ function tabController(event) {
     }
     //ESC key
     if(event.keyCode == 27) {
-        console.log('closing overlay');
-        closeoverlay();
+        console.log('registered esc key');
     }
 }
 
-//create an array of primary navigation sections on the site
-function setupPrimaryNav() {
+a11yController.prototype.setupPrimaryNav = function () {
     //main ui sections
     var blockEditor = document.querySelector('.droplet-main-scroller')
     var blockCategory = document.querySelector('.droplet-palette-header')    
@@ -61,11 +56,16 @@ function setupPrimaryNav() {
         consoleInput
     ]
 
+    //remove tab index from primary navigation so we can handle this manually
+    primaryNavSections.forEach(function(element) {
+        element.setAttribute("tabindex", -1);
+    })
+
     return primaryNavSections;
 }
 
 //bootstrap a11y enhancements after window loads
 window.addEventListener('load', function load(event){
-    window.removeEventListener('load', load, false);
-    _init();
+    window.removeEventListener('load', load, false);    
+    var a11yEnhancement = new a11yController();
 })
